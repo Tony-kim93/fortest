b49eb0edbc23345ef6557bef6e4171d9b7252c9c
Tony <Tony@tucks-MacBook-Pro.local>
2021-04-23 09:54:21 +0900

    hihi

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>adfasfdasfsa</div>
         <a
           className="App-link"
           href="https://reactjs.org"
